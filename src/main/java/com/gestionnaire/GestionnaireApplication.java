a00e993a01aa619e956a630861993d4cd02eb7ba
Majda <echabbimajda2001@gmail.com>
2026-03-07 00:13:47 +0000

    Initial commit

---
+++ src/main/java/com/gestionnaire/GestionnaireApplication.java
@@ -0,0 +1,12 @@
+package com.gestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gestionnaire/model/package-info.java
@@ -0,0 +1,55 @@
+package com.gestionnaire.model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "comptes")
+public class Compte {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   
+    @NotBlank(message = "Le site web est obligatoire")
+    @Pattern(regexp = "^https://.*", message = "L'URL du site web doit commencer par https")
+    private String siteWeb;
+
+    @NotBlank(message = "L'identifiant est obligatoire")
+    private String identifiant;
+
+  
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String motDePasseChiffre;
+
+    private String notePrivée;
+
+    
+    public Compte() {}
+
+    public Compte(String siteWeb, String identifiant, String motDePasseChiffre, String notePrivée) {
+        this.siteWeb = siteWeb;
+        this.identifiant = identifiant;
+        this.motDePasseChiffre = motDePasseChiffre;
+        this.notePrivée = notePrivée;
+    }
+
+   
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getSiteWeb() { return siteWeb; }
+    public void setSiteWeb(String siteWeb) { this.siteWeb = siteWeb; }
+
+    public String getIdentifiant() { return identifiant; }
+    public void setIdentifiant(String identifiant) { this.identifiant = identifiant; }
+
+    public String getMotDePasseChiffre() { return motDePasseChiffre; }
+    public void setMotDePasseChiffre(String motDePasseChiffre) { this.motDePasseChiffre = motDePasseChiffre; }
+
+    public String getNotePrivée() { return notePrivée; }
+    public void setNotePrivée(String notePrivée) { this.notePrivée = notePrivée; }
+}
